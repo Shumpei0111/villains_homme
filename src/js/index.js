e528f14002ed957b5f1d86378f6db632d9692199
Shumepi <shumpei.o.7g@gmail.com>
2021-10-09 18:18:17 +0900

    Update ファーストビューの画像差し替え、キャッチを画像化、その他CSSのアップデート

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
 import fadeIn from './fadein.js';
 
 fadeIn();
+
+const fvFadeIn = () => {
+    const $tgt = document.getElementById( 'fvAnimationTgt' );
+    $tgt.classList.add( 'active' );
+}
+
+setTimeout( () => {
+    fvFadeIn();
+}, 100 )
